d65c1a97278dd21db86f03a7c88436f48b351a99
mnimje <mnimje@github.com>
2016-10-10 10:34:49 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     var myEducation = $("#myEducation").position();
     var myPortfolio = $("#myPortfolio").position();
     var myContact = $("#myContact").position();
+    var myFeedback = $("#myFeedback").position();
     
     $(".caption").css("width","30%");
  
@@ -20,16 +21,22 @@ $(document).ready(function(){
         
       if (y > 220) {
         $(".headerNavBar").css({"margin-left":"-65px","background":"#DC3248"});
-        $(".headerNavBar a").css('color','white !important');
+        $(".headerNavBar a").css('color','white !important');        
       }
         else {
         $(".headerNavBar").css({"margin-left":"0px","background":"white"});	
         $(".headerNavBar a").css('color','black !important');
       }
-                        
-      var test = (aboutMe.top < y ) ? drawCaption(y,aboutMe.top,"About Me","about","","about") : "" ;   
         
-      if(aboutMe.top < y ){
+        if(y > (aboutMe.top -10 )){
+            drawCaption(y,aboutMe.top,"About Me","About Me","fixed","","about");
+        }else{
+            drawCaption(y,aboutMe.top,"About Me","About Me","","fixed","about");
+        }
+                        
+      //var test = (aboutMe.top < y ) ? drawCaption(y,aboutMe.top,"About Me","about","","about") : "" ;   
+        
+      /*if(aboutMe.top < y ){
         drawCaption(y,aboutMe.top,"About Me","About Me","about","","about");  
       }else {
         drawCaption(y,aboutMe.top,"About Me","About Me","about","fixed","about");
@@ -38,7 +45,32 @@ $(document).ready(function(){
         drawCaption(y,mySkills.top,"My Skills","About Me","mySkills","about","about");  
       }else{        
         drawCaption(y,mySkills.top,"My Skills","About Me","about","mySkills","about");  
-      }
+      }*/
+        
+        if( y >= 0 && mySkills.top > y ){    
+            //console.log("about me");
+            drawCaption(y,aboutMe.top,"About Me","","about","","about");  
+        }else if(y > (mySkills.top - 10) && myExp.top > y){
+            //console.log("my Skills");        
+            drawCaption(y,mySkills.top,"My Skills","About Me","mySkills","about","about");
+        }else if(y > (myExp.top - 10) && myEducation.top > y){
+            //console.log("my Experience");        
+            drawCaption(y,myExp.top,"My Experience","My Skills","myExperience","mySkills","about");
+        }else if(y > (myEducation.top - 10) && myPortfolio.top > y){
+            //console.log("my Edu");        
+            drawCaption(y,myEducation.top,"My Education","My Experience","myEducation","myExperience","about");
+        }else if(y > (myPortfolio.top - 10) && myContact.top > y){
+            //console.log("my port");        
+            drawCaption(y,myPortfolio.top,"My Portfolio","My Education","myPortfolio","myEducation","about");
+        }else if(y > (myContact.top - 10) && myFeedback.top > y){
+            //console.log("my Contact");        
+            drawCaption(y,myContact.top,"My Contact","My Education","myContact","myPortfolio","about");
+        }else{
+            //console.log("my feed");
+            drawCaption(y,myFeedback.top,"Feedback","My Contact","myFeedback","myContact","about");
+        }
+        
+        
               
     });
 
@@ -51,12 +83,15 @@ $(document).ready(function(){
 */
 
 function drawCaption(scrollPos,contentPos,textToShow,textToChange,classToAdd,classToRemove,id){
-    if(contentPos < scrollPos){
+    /*if(contentPos < scrollPos){
         $("#"+id).addClass("fixed");
         $("#"+id).addClass(classToAdd).removeClass(classToRemove);
         $("#"+id).find(".innerText").text(textToShow);
     }else{
         $("#"+id).removeClass(classToRemove).addClass(classToAdd);
         $("#"+id).find(".innerText").text(textToChange);
-    }
+    }*/
+    $("#"+id).removeClass("about mySkills myExperience myPortfolio myEducation myContact myFeedback");
+    $("#"+id).addClass(classToAdd).removeClass(classToRemove);
+    $("#"+id).find(".innerText").text(textToShow);
 }
